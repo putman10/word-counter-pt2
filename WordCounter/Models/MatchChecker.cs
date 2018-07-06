03081f46e063bcc216a524846c11430fa617e5f4
Ryan Putman <putman10@me.com>
2018-07-06 16:25:04 -0700

    change model tests to be relevant and pass after new functionality

--- WordCounter/Models/MatchChecker.cs
+++ WordCounter/Models/MatchChecker.cs
@@ -88,7 +88,7 @@ namespace WordCounter
                 }
             }
             SetCleanList(list.ToLowerInvariant());
-            return list;
+            return list.ToLowerInvariant();
         }
 
 
