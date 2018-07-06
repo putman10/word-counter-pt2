11bc08af9f04e702a7fdf95a64cf25a765e4b51b
Ryan Putman <putman10@me.com>
2018-07-06 16:41:47 -0700

    add custom static styling

---
+++ WordCounter.Tests/ControllerTests/WordCounterControllerTest.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using WordCounter.Controllers;
+using WordCounter;
+
+namespace WordCounter.Tests
+{
+    [TestClass]
+    public class ResultsControllerTest
+    {
+        [TestMethod]
+        public void Index_ReturnsCorrectView_True()
+        {
+            //Arrange
+            ResultsController controller = new ResultsController();
+
+            //Act
+            ActionResult indexView = controller.Results("dog", "dog dog");
+
+            //Assert
+            Assert.IsInstanceOfType(indexView, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void Index_HasCorrectModelType_ItemList()
+        {
+            //Arrange
+            ViewResult indexView = new ResultsController().Results("dog", "dog dog") as ViewResult;
+
+            //Act
+            var result = indexView.ViewData.Model;
+
+            //Assert
+            Assert.IsInstanceOfType(result, typeof(string));
+        }
+    }
+}
