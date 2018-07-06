1260c6deb1db55545409aa9d49fa2a2a0b321a8c
Ryan Putman <putman10@me.com>
2018-07-06 11:00:36 -0700

    change naming conventions and bring in code from prior week

--- WordCounter/Controllers/HomeController.cs
+++ WordCounter/Controllers/HomeController.cs
@@ -3,9 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using MadLibsGame.Models;
+using WordCounter;
 
-namespace MadLibsGame.Controllers
+namespace WordCounter.Controllers
 {
     public class HomeController : Controller
     {
@@ -18,18 +18,10 @@ namespace MadLibsGame.Controllers
         [Route("/word_counter_results")]
         public ActionResult WordCounterResults()
         {
-            MadLibs newGame = new MadLibs();
-            newGame.SetOccupation(Request.Query["occupation"]);
-            newGame.SetNounOne(Request.Query["nounOne"]);
-            newGame.SetNounTwo(Request.Query["nounTwo"]);
-            newGame.SetNounThree(Request.Query["nounThree"]);
-            newGame.SetNounFour(Request.Query["nounFour"]);
-            newGame.SetAdjectiveOne(Request.Query["adjectiveOne"]);
-            newGame.SetAdjectiveTwo(Request.Query["adjectiveTwo"]);
-            newGame.SetVerbOne(Request.Query["verbOne"]);
-            newGame.SetVerbTwo(Request.Query["verbTwo"]);
-            newGame.SetVerbThree(Request.Query["verbThree"]);
-            return View(newGame);
+            RepeatCounter newCounter = new RepeatCounter();
+            newCounter.SetOccupation(Request.Query["singleWord"]);
+            newCounter.SetNounOne(Request.Query["listOfWords"]);
+            return View(newCounter);
         }
     }
 }
---
+++ WordCounter/Models/MatchChecker.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace WordCounter
+{
+    public class RepeatCounter
+    {
+
+        private string _occupation;
+        private string _nounOne;
+
+
+        public string GetOccupation()
+        {
+            return _occupation;
+        }
+
+        public void SetOccupation(string newOccupation)
+        {
+            _occupation = newOccupation;
+        }
+
+        public string GetNounOne()
+        {
+            return _nounOne;
+        }
+
+        public void SetNounOne(string newNounOne)
+        {
+            _nounOne = newNounOne;
+        }
+        
+        public string RemoveSpacesandCharactersFromSingleWord(string inputOne)
+        {
+            char[] charsToTrim = { ',', '{', ' ', '~', '`', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+', '=' };
+            string singleInputWithoutCharacters = inputOne.Trim(charsToTrim);
+            ConvertSingleWordToLowerCase(singleInputWithoutCharacters);
+            return singleInputWithoutCharacters;
+        }
+
+        public string RemoveCharactersFromStringWords(string inputTwo)
+        {
+            string[] chars = new string[] { ",", ".", "/", "!", "@", "#", "$", "%", "^", "&", "*", "'", "\"", ";", "_", "(", ")", ":", "|", "[", "]" };
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (inputTwo.Contains(chars[i]))
+                {
+                    inputTwo = inputTwo.Replace(chars[i], "");
+                }
+            }
+            ConvertStringOfWordsToLowerCase(inputTwo);
+            return inputTwo;
+        }
+
+        public string ConvertSingleWordToLowerCase(string inputOne)
+        {
+            string lowerCaseInputOne = inputOne.ToLowerInvariant();
+            return lowerCaseInputOne;
+        }
+
+        public string ConvertStringOfWordsToLowerCase(string inputTwo)
+        {
+            string lowerCaseInputTwo = inputTwo.ToLowerInvariant();
+            return lowerCaseInputTwo;
+        }
+
+        public int Final(string inputOne, string inputTwo)
+        {
+
+            var arr = inputTwo.Split(new char[] { ' ', '.', ',', ';', ':', '?', '!' });
+            var count = Array.FindAll(arr, s => s.Equals(inputOne.Trim())).Length;
+            return count;
+
+        }
+    }
+}
